5f9bad006445ddc09b4f452c5a3135208002c1a7
Debnath Sinha <debnathsinha@gmail.om>
2015-05-07 19:26:39 +0530

    First pass at writing into and reading from the DataStore, almost empty quiz model!

--- qwiznation.py
+++ qwiznation.py
@@ -1,6 +1,7 @@
 import webapp2
 import os
 import jinja2
+from google.appengine.ext import ndb
 
 ROOT_PATH = os.path.dirname(__file__)
 GMAILYTICS_TEMPLATE_PATH = os.path.join(ROOT_PATH,"templates") 
@@ -9,12 +10,39 @@ JINJA_ENV = jinja2.Environment(
                                 extensions=['jinja2.ext.autoescape'],
                                 autoescape=True)
 
+class Quiz(ndb.Model):
+    name = ndb.StringProperty()
+    created_time = ndb.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        quizes = Quiz.query().fetch()
+        values = { 
+            'quizes': quizes
+        }
         template = JINJA_ENV.get_template("index.html")
-        self.response.write(template.render())
+        self.response.write(template.render(values))
+
+    def post(self):
+        quiz = Quiz(name=self.request.get('message'))
+        quiz.put()
+        self.redirect('/')
+
+class QuizPage(webapp2.RequestHandler):
+    def get(self):
+        pass
+
+class QuestionPage(webapp2.RequestHandler):
+    def get(self):
+        pass
+
+class AnswerPage(webapp2.RequestHandler):
+    def get(self):
+        pass
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    (r'/api/quiz/', QuizPage),
+    (r'/api/question/', QuestionPage),
+    (r'/api/answer/', AnswerPage)
 ], debug=True)
